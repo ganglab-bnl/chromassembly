d4fa44f85c5a4ee8526588db2bbc62ac8ca390b9
sarah <ssh2198@columbia.edu>
2024-06-06 04:50:42 -0400

    Beautiful refactor

---
+++ LatticeVisualizer.py
@@ -0,0 +1,102 @@
+import sys
+import numpy as np
+from vispy import app, scene
+from vispy.color import Color
+
+class LatticeVisualizer:
+    """
+    Class to help visualize the input lattice.
+    """
+    def __init__(self, lattice):
+
+        self.lattice = lattice
+        self.n_layers, self.n_rows, self.n_columns = lattice.shape
+        
+        self.canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
+        self.view = self.canvas.central_widget.add_view()
+        self.view.bgcolor = '#efefef'
+        self.view.camera = scene.cameras.ArcballCamera(fov=0)
+        self.view.camera.scale_factor = 500
+
+        self.colordict = {
+            0: Color("#AAAAAA80"),
+            1: Color("#3781A9"),
+            2: Color("#57ACC1"),
+            3: Color("#7ECD61"),
+            4: Color("#BBE355"),
+            5: Color("#F9E273"),
+            6: Color("#EAAB83"),
+            7: Color("#DC758F"),
+        }
+
+        self.p_dist = 40 # Distance between each visualized particle (ball)
+
+    def run(self):
+        """
+        Visualize the lattice in 3D space.
+        """
+        #TODO: implement diff b/w interactive and non-interactive (add when needed)
+
+        # Create a dictionary of random colors for unspecified materials 8-10
+        # (can add more or less later)
+        for i in range(8, 10):
+            color = Color((np.random.rand(), np.random.rand(), np.random.rand()))
+            color.alpha = 1.0
+            self.colordict[i] = color
+
+        # Determine the coordinates and colors of each voxel (tbe ball / particle) in lattice
+        lattice_coords = []
+        lattice_colors = []
+        for layer in range(self.n_layers):
+            for row in range(self.n_rows):
+                for column in range(self.n_columns):
+                    # Set x, y, z coordinates (to plot) for each voxel
+                    x = layer * self.p_dist
+                    y = row * self.p_dist
+                    z = column * self.p_dist
+                    
+                    voxel_material = self.lattice[layer, row, column]
+                    voxel_color = self.colordict[voxel_material]
+                    lattice_colors.append(voxel_color.rgba)
+                    lattice_coords.append((x, y, z))
+                    # print(f'Voxel at ({x}, {y}, {z}) has color {color.rgba}')
+
+        lattice_coords = np.concatenate([lattice_coords, [[0, 0, 0]]], axis=0)
+        lattice_colors = np.concatenate([lattice_colors, [[1, 0, 0, 1]]] * (len(lattice_coords) - len(lattice_colors)), axis=0)
+
+        vispyViewer = scene.visuals.Markers(
+            pos = lattice_coords,
+            size = 20,
+            antialias=0,
+            face_color=lattice_colors,
+            edge_color='white',
+            edge_width=0,
+            scaling=True,
+            spherical=True
+        )
+        vispyViewer.parent = self.view.scene
+        
+        # Create tubes to connect the balls
+        for layer in range(self.n_layers):
+            for row in range(self.n_rows):
+                for column in range(self.n_columns):
+                    # Same column and row but different layers
+                    if layer < self.n_layers - 1:  
+                        start = (layer * self.p_dist, row * self.p_dist, column * self.p_dist)
+                        end = ((layer + 1) * self.p_dist, row * self.p_dist, column * self.p_dist)
+                        tube = scene.visuals.Tube(np.array([start, end]), color=self.colordict[0].rgba)
+                        tube.parent = self.view.scene
+                    # Same layer and column but different rows
+                    if row < self.n_rows - 1:  
+                        start = (layer * self.p_dist, row * self.p_dist, column * self.p_dist)
+                        end = (layer * self.p_dist, (row + 1) * self.p_dist, column * self.p_dist)
+                        tube = scene.visuals.Tube(np.array([start, end]), color=self.colordict[0].rgba)
+                        tube.parent = self.view.scene
+                    # Same layer and row but different columns
+                    if column < self.n_columns - 1:  
+                        start = (layer * self.p_dist, row * self.p_dist, column * self.p_dist)
+                        end = (layer * self.p_dist, row * self.p_dist, (column + 1) * self.p_dist)
+                        tube = scene.visuals.Tube(np.array([start, end]), color=self.colordict[0].rgba)
+                        tube.parent = self.view.scene
+
+        app.run()
--- Voxel.py
+++ Voxel.py
@@ -1,109 +1,210 @@
 import numpy as np
 from LatticeCreator import LatticeCreatorGUI
+from math import ceil
 
 class Voxel:
-    def __init__(self, number, material, coordinates, unit_cell):
+    def __init__(self, material, coordinates, index):
         """
-        Each voxel contains the following information handles:
-            - number: Voxel number
-            - material: Particle type corresponding to a material
-                - 0: empty
-                - 1, 2, 3, etc.
-            - coordinates: Voxel's coordinates in the user's unit_cell matrix (a x b x c)
-            - neighbors: List of direct neighboring voxels in input unit_cell matrix
+        A Voxel object containing a particular material 
+        and location in the desired Lattice.UnitCell.
+        @param:
+            - material: int, the material cargo of a voxel
+                -> 0: empty
+                -> 1=Silver, 2=Gold, 3=(...) etc.
+            - coordinates: Voxel's coordinates in the Lattice.MinDesign np.array
+            - index: Voxel's index in the Lattice.voxels list
+        """
+        self.material = material
+        self.coordinates = coordinates
+        self.index = index
+
+        """
+        @old-param (maybe not needed):
+            - neighbors: List of direct neighboring voxels in UnitCell
                 +x, -x, +y, -y, +z, -z
             - bind_partners: List of current binding neighbors in each direction; 
-                             value is index of neighbor in unit_cell
+                             value is index of neighbor in UnitCell
                 +x, -x, +y, -y, +z, -z
             - bind_colors: List of current binding colors in each direction
                 +x, -x, +y, -y, +z, -z
             - bparam: List of binding parameters, including binding color and complementarity
             - symmetry: List of all voxels it possesses symmetry with
                 - Ordered as [sym_voxel, sym_element(s)]
-            
         """
-        
-        # Fill in material characteristics
-        self.number = number
-        self.material = material
-        self.coordinates = coordinates
-
-        # To be filled in later
         self.neighbors = []
         self.bind_partners = []
         self.bind_colors = []
         self.bparam = []
 
         # Surrounding matrix
-        self.surround_matrix = np.zeros_like(unit_cell)
+        # self.surround_matrix = np.zeros_like(unit_cell)
         
     def set_surround_matrix(self, unit_cell):
         """
         Set the surrounding matrix for the voxel.
         """
-        nlay, nrow, ncol = np.shape(unit_cell)
-        nrepeat = 3 # Builds surroundings to be +/- 1 unit cell in each direction
-        o = int(floor(nrepeat/2)*(nlay-1))
-        m = int(floor(nrepeat/2)*(nrow-1))
-        p = int(floor(nrepeat/2)*(ncol-1))
-
-        # For each voxel in the unit cell
-        for i in range(nlay):
-            for j in range(nrow):
-                for k in range(ncol):
-
-                    self.surround_matrix[i,j,k] = unit_cell[i,j,k]
+        n_lay, n_rows, n_col = np.shape(unit_cell)
+        
 
 
-class UnitCell:
-    def __init__(self, lattice):
+class Bond:
+    def __init__(self, voxel1, voxel2, color):
         """
-        A matrix of voxels forming the unit cell in 3D space.
         @param:
-            - lattice: 3D-numpy array of voxels
-        Note the right-most column and the bottom-most layer are the repeated boundaries.
+            - voxel1:   Voxel object
+            - voxel2:   Voxel object
+            - color:    Bond color
         """
-        self.lattice = lattice
-        self.voxels = self.convert_np_to_voxels(self.lattice)
+        self.voxel1 = voxel1
+        self.voxel2 = voxel2
+        self.color = color
 
-    # def find_repeat_layers(self, lattice):
-    #     """
-    #     Find the layers that are repeated in the lattice.
-    #     """
-    #     repeat_z_layers = []
-    #     for i in range(np.shape(lattice)[0]):
-    #         if 
-    #     return repeat_z_layers
     
-    def convert_np_to_voxels(self, lattice):
+
+class Lattice:
+    def __init__(self, input_lattice):
+        """Initialize a matrix of voxels forming the unit cell in 3D space.
+        @param:
+            - input_lattice:  3D np.array of ints (direct output from LatticeCreator)
+        
+        Attributes:
+            - voxels: List of Voxel objects in the unit cell
+            - UnitCell: 3D np.array of ints, has repeated x, y, z layers
+            - MinDesign: 3D np.array of ints, minimum copy-pastable design
+                -> ints := Voxel.material
+        """
+        # Was user's design already a unit cell?
+        is_unit_cell = self.isUnitCell(input_lattice)
+
+        if is_unit_cell:
+            self.UnitCell = input_lattice
+            self.MinDesign = input_lattice[:-1, :-1, :-1] # Remove repeated layers
+        elif not is_unit_cell:
+            self.UnitCell = np.pad(input_lattice, ((0, 1), (0, 1), (0, 1)), 'wrap') # Repeat layers
+            self.MinDesign = input_lattice
+
+        self.voxels = self.initVoxelList(self.MinDesign)
+    
+
+    def initVoxelList(self, MinDesign):
         """
         Convert a numpy array to a list of voxels.
+        @param:
+            - MinDesign: 3D numpy array of ints, the minimum copy-pastable design
+        @return:
+            - voxels: Ordered list of Voxel objects
         """
         voxels = []
-        count = 0
-        for i in range(np.shape(lattice)[0]):
-            for j in range(np.shape(lattice)[1]):
-                for k in range(np.shape(lattice)[2]):
-                    # Create and append voxel to list
-                    vox_coord = (i, j, k)
-                    vox = Voxel(count, lattice[i,j,k], vox_coord, lattice)
-                    voxels.append(vox)
-                    count += 1
+        index = 0
+        for coordinate, value in np.ndenumerate(MinDesign):
+            # Create and append voxel with value(material) and index(coordinates)
+            current_voxel = Voxel(value, coordinate, index)
+            voxels.append(current_voxel)
+            index += 1
         return voxels
+
+
+    def isUnitCell(self, lattice):
+        """
+        Returns whether a given lattice (np.array) is a unit cell.
+        @param:
+            - lattice: 3D numpy array of ints (direct output from LatticeCreator)
+        @return:
+            - is_unit_cell: bool
+        """
+        x_repeat, y_repeat, z_repeat = False, False, False
+        is_unit_cell = False
+        
+        # Check if z, x, or y layers are repeated
+        if np.array_equal(lattice[0, :, :], lattice[-1, :, :]):
+            z_repeat = True
+        if np.array_equal(lattice[:, 0, :], lattice[:, -1, :]):
+            x_repeat = True
+        if np.array_equal(lattice[:, :, 0], lattice[:, :, -1]): 
+            y_repeat = True
+        
+        # If all layers are repeated, the lattice is a unit cell
+        if x_repeat and y_repeat and z_repeat:
+            is_unit_cell = True
+        
+        return is_unit_cell
+    
+
+    def reduceIfUnitCell(self, lattice, is_unit_cell):
+        """
+        Remove repeated layers from a lattice if it is a unit cell.
+        @param:
+            - lattice: 3D numpy array of ints
+            - is_unit_cell: bool
+        @return:
+            - lattice: 3D numpy array of ints, either reduced or the same as input
+        """
+        if is_unit_cell:
+            # Remove repeated layers
+            lattice = lattice[:-1, :-1, :-1]
+        return lattice
+    
+    def addRepeatLayers(self, lattice):
+        """
+        Add repeated layers to a lattice to make it a unit cell.
+        @param:
+            - lattice: 3D numpy array of ints
+        @return:
+            - lattice: 3D numpy array of ints, with repeated layers
+        """
+        lattice = np.pad(lattice, ((0, 1), (0, 1), (0, 1)), 'wrap')
+        return lattice
+    
+
+class SurroundingsManager:
+    def __init__(self, lattice: Lattice):
+        """
+        Manager class for creating, transforming, and comparing VoxelSurroundings
+        matrices for a given lattice design."""
+        self.FullSurroundings = self.initFullSurroundings(lattice)
+
+    def initFullSurroundings(self, lattice: Lattice):
+        """
+        Initialize a FullSurroundings for all voxels in Lattice.MinDesign,
+        which all other SurroundingsMatrices will be subset from.
+        @param:
+            - lattice: A Lattice object
+        @return:
+            - full_surroundings: 3D numpy array of ints
+        """
+        max_dim = max(np.shape(lattice.MinDesign))
+        final_dim = 3 * max_dim
+
+        repeats = [ceil(final_dim / dim) for dim in lattice.MinDesign.shape]
+        full_surroundings = np.tile(lattice.MinDesign, repeats)
+
+        return full_surroundings
+
+
+    def getVoxelSurroundings(self, voxel: Voxel):
+        """
+        Get the SurroundingsMatrix for a given voxel in the UnitCell, which stores 
+        tuples (voxel.material, voxel.index) for each value in the SurroundingsMatrix.
+        @param:
+            - voxel: The Voxel object to build SurroundingsMatrix around
+            - unit_cell: The UnitCell object
+        @return:
+            - surroundings_matrix: 3D numpy array of tuples (voxel.material, voxel.index) 
+        """
+        return
 
 
 if __name__ == '__main__':
     # Create a unit cell
-    lattice, isUnitCell = LatticeCreatorGui().run()
-    unit_cell = UnitCell(lattice)
+    input_lattice, isUnitCell = LatticeCreatorGUI().run()
+    lattice = Lattice(input_lattice)
+    SM = SurroundingsManager(lattice)
 
-    print(f'Original lattice:\n{lattice}\n')
-    for voxel in unit_cell.voxels:
-        voxel.set_surround_matrix(lattice)
-        print(f'\nVoxel {voxel.number} has surrounding matrix:')
-        print(voxel.surround_matrix)
-        print('\n')
-        break # only do once lol
-    
-    print(f'Lattice shape: {np.shape(lattice)}')
-    
+    # Test
+    print(f"Is this a unit cell? {lattice.isUnitCell(input_lattice)}\n")
+
+    print(f"Unit cell:\n{lattice.UnitCell}\n")
+
+    print(f"Min design:\n{lattice.MinDesign}\n")
+
+    print(f"Full surroundings:\n{SM.FullSurroundings}\n")
