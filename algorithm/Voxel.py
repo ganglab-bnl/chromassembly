71201702934d502385e8a85fd3b0a06c5a9f149e
sarah <ssh2198@columbia.edu>
2024-06-06 17:05:07 -0400

    Surround Sound (matrix)

--- algorithm/Voxel.py
+++ algorithm/Voxel.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import ceil
+from math import ceil, floor
 
 class Voxel:
     def __init__(self, material, coordinates, index):
@@ -159,23 +159,45 @@ class SurroundingsManager:
     def __init__(self, lattice: Lattice):
         """
         Manager class for creating, transforming, and comparing VoxelSurroundings
-        matrices for a given lattice design."""
+        matrices for a given lattice design.
+        """
+        self.max_len = max(np.shape(lattice.MinDesign))
+        self.lattice = lattice # ref to Lattice object
+
         self.FullSurroundings = self.initFullSurroundings(lattice)
 
     def initFullSurroundings(self, lattice: Lattice):
         """
-        Initialize a FullSurroundings for all voxels in Lattice.MinDesign,
+        Initialize a FullSurroundings from all voxels in Lattice.MinDesign,
         which all other SurroundingsMatrices will be subset from.
         @param:
             - lattice: A Lattice object
         @return:
             - full_surroundings: 3D numpy array of ints
         """
-        max_dim = max(np.shape(lattice.MinDesign))
-        final_dim = 3 * max_dim
+        max_len = max(np.shape(lattice.MinDesign))
+        final_len = 3 * max_len
 
-        repeats = [ceil(final_dim / dim) for dim in lattice.MinDesign.shape]
-        full_surroundings = np.tile(lattice.MinDesign, repeats)
+        # Get dimensions of MinDesign (we are tiling MinDesign)
+        z_len, x_len, y_len = lattice.MinDesign.shape
+
+        # I imagine z=layers, x=rows, y=columns
+        z_repeats = ceil(final_len / z_len)
+        x_repeats = ceil(final_len / x_len) 
+        y_repeats = ceil(final_len / y_len) 
+
+        # If x, y, or z repeats are even, add 1 to make them odd
+        if z_repeats % 2 == 0:
+            z_repeats += 1
+        if x_repeats % 2 == 0:
+            x_repeats += 1
+        if y_repeats % 2 == 0:
+            y_repeats += 1
+
+        print(f'z_repeats: {z_repeats}, x_repeats: {x_repeats}, y_repeats: {y_repeats}')
+
+        self.repeats = [z_repeats, x_repeats, y_repeats]
+        full_surroundings = np.tile(lattice.MinDesign, self.repeats)
 
         return full_surroundings
 
@@ -190,21 +212,59 @@ class SurroundingsManager:
         @return:
             - surroundings_matrix: 3D numpy array of tuples (voxel.material, voxel.index) 
         """
-        return
+        # if self.md_index == 0:
+        #surround_dim = +/- (max_dim/2) in all xyz directions, not including voxel
+
+        # How far down to go in each direction
+        og_zlen, og_xlen, og_ylen = np.shape(self.lattice.MinDesign)
+        z_repeats, x_repeats, y_repeats = self.repeats
+
+        # Get halfway points for each direction
+        # (x_0, y_0, z_0) corresponds to the (0,0,0) position of the middle MinDesign
+        z_0 = floor(z_repeats / 2) * og_zlen
+        x_0 = floor(x_repeats / 2) * og_xlen
+        y_0 = floor(y_repeats / 2) * og_ylen
+
+        # Get voxel's new coordinates within middle MinDesign
+        vox_z = z_0 + voxel.coordinates[0]
+        vox_x = x_0 + voxel.coordinates[1]
+        vox_y = y_0 + voxel.coordinates[2]
+
+        max_og_len = max(og_zlen, og_xlen, og_ylen)
+        extend_amt = floor(max_og_len / 2)
+
+        VoxelSurrounding = self.FullSurroundings[(vox_z-extend_amt) : (vox_z+extend_amt+1),
+                                                 (vox_x-extend_amt) : (vox_x+extend_amt+1),
+                                                 (vox_y-extend_amt) : (vox_y+extend_amt+1)]
+
+        return VoxelSurrounding
 
 
 if __name__ == '__main__':
+    # Note: To run this file, cd into /algorithm and run 'python3 Voxel.py'
+    # (sys.path.append ignores customtkinter otherwise)
+    import sys
+    sys.path.append('..')
     from visualizations.LatticeCreator import LatticeCreatorGUI
+    from visualizations.LatticeVisualizer import LatticeVisualizer
+
     # Create a unit cell
     input_lattice, isUnitCell = LatticeCreatorGUI().run()
     lattice = Lattice(input_lattice)
-    SM = SurroundingsManager(lattice)
+    sm = SurroundingsManager(lattice)
 
     # Test
-    print(f"Is this a unit cell? {lattice.isUnitCell(input_lattice)}\n")
-
-    print(f"Unit cell:\n{lattice.UnitCell}\n")
+    # print(f"Is this a unit cell? {lattice.isUnitCell(input_lattice)}\n")
+    # print(f"Unit cell:\n{lattice.UnitCell}\n")
 
     print(f"Min design:\n{lattice.MinDesign}\n")
 
-    print(f"Full surroundings:\n{SM.FullSurroundings}\n")
+    print(f"Full surroundings:\n{sm.FullSurroundings}\n")
+    print(f'FullSurroundings shape: {np.shape(sm.FullSurroundings)}')
+    print(f'z_repeats: {sm.repeats[0]}, x_repeats: {sm.repeats[1]}, y_repeats: {sm.repeats[2]}')
+
+    # Test getVoxelSurroundings
+    voxel = lattice.voxels[0]
+    print(f"Voxel 0: {voxel.coordinates}, material: {voxel.material}\n")
+    voxel_surroundings = sm.getVoxelSurroundings(voxel)
+    print(f"Voxel 0 surroundings:\n{voxel_surroundings}\n")
